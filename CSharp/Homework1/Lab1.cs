a6e4790061d1c86608292b62cb6de8981422bdeb
ADM <parfemax@gmail.com>
2023-06-07 01:15:48 +0300

    Homework 3

---
+++ CSharp/Homework1/Lab1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Homework1
+{
+    public class Lab1
+    {
+        static void Start(string[] args)
+        {
+            task1();
+
+            task2();
+
+            task3();
+
+            task4();
+
+            task5();
+        }
+
+        static void task1()
+        {
+            Console.Write("Enter symbols: ");
+            string input = Console.ReadLine();
+
+            foreach (char letter in input)
+            {
+                int lowercaseA = (int)'a';
+                int uppercaseA = (int)'A';
+
+                // Знаходимо позицію літери в алфавіті
+                int position = (int)char.ToLower(letter) - lowercaseA + 1;
+
+                // Перекладаємо букву на інший регістр
+                char convertedLetter;
+                if (char.IsLower(letter))
+                {
+                    convertedLetter = (char)((int)letter - lowercaseA + uppercaseA);
+                }
+                else
+                {
+                    convertedLetter = (char)((int)letter - uppercaseA + lowercaseA);
+                }
+
+                Console.WriteLine("Character: {0} Alphabetical position: {1} Letters in other case: {2}", letter, position, convertedLetter);
+                Console.ReadLine();
+            }
+        }
+
+        static void task2()
+        {
+            string input = "London, Paris, Rome";
+            char delimiter = ',';
+
+            List<string> substrings = new List<string>();
+            int start = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == delimiter)
+                {
+                    string substring = input.Substring(start, i - start);
+                    substrings.Add(substring.Trim());
+                    start = i + 1;
+                }
+            }
+
+            // Додаємо останній підряд після останнього роздільника
+            string lastSubstring = input.Substring(start);
+            substrings.Add(lastSubstring.Trim());
+
+            foreach (string substring in substrings)
+            {
+                Console.WriteLine(substring);
+            }
+        }
+
+        static void task3()
+        {
+            Console.WriteLine("Enter the string:");
+            string mainString = Console.ReadLine();
+
+            Console.WriteLine("Enter a substring to search for:");
+            string subString = Console.ReadLine();
+
+            bool containsSubstring = CheckSubstring(mainString, subString);
+            if (containsSubstring)
+            {
+                Console.WriteLine("Substring found in string.");
+            }
+            else
+            {
+                Console.WriteLine("Substring not found in string.");
+            }
+        }
+        static bool CheckSubstring(string mainString, string subString)
+        {
+            int mainLength = mainString.Length;
+            int subLength = subString.Length;
+
+            for (int i = 0; i <= mainLength - subLength; i++)
+            {
+                int j;
+                for (j = 0; j < subLength; j++)
+                {
+                    if (mainString[i + j] != subString[j])
+                    {
+                        break;
+                    }
+                }
+
+                if (j == subLength)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        static string[] units = { "", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "десять", "одиннадцать", "двенадцать", "тринадцать", "четырнадцать", "пятнадцать", "шестнадцать", "семнадцать", "восемнадцать", "девятнадцать" };
+        static string[] tens = { "", "", "двадцать", "тридцать", "сорок", "пятьдесят", "шестьдесят", "семьдесят", "восемьдесят", "девяносто" };
+        static string[] hundreds = { "", "сто", "двести", "триста", "четыреста", "пятьсот", "шестьсот", "семьсот", "восемьсот", "девятьсот" };
+        static void task4()
+        {
+            int number = 117;
+            string words = NumberToWords(number);
+            Console.WriteLine($"{number} - {words}");
+        }
+
+        static string NumberToWords(int number)
+        {
+            if (number == 0)
+            {
+                return "ноль";
+            }
+
+            // Розбиваємо число на окремі розряди
+            int digit1 = number / 100;
+            int digit2 = (number / 10) % 10;
+            int digit3 = number % 10;
+
+            string result = "";
+
+            if (digit1 != 0)
+            {
+                result += hundreds[digit1] + " ";
+            }
+            if (digit2 != 0)
+            {
+                if (digit2 == 1)
+                {
+                    result += units[digit2 * 10 + digit3] + " ";
+                }
+                else
+                {
+                    result += tens[digit2] + " ";
+                }
+            }
+            if (digit3 != 0 && digit2 != 1)
+            {
+                result += units[digit3] + " ";
+            }
+
+            return result.Trim();
+        }
+
+        static void task5()
+        {
+            int a = 5;
+            int b = 10;
+
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+
+            Console.WriteLine("a = " + a); // Виводить 10
+            Console.WriteLine("b = " + b); // Виводить 5
+        }
+    }
+}
---
+++ CSharp/Homework3/Game_card.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Homework3
+{
+
+    public class Game_card
+    {
+        enum Suit
+        {
+            Spades,
+            Hearts,
+            Diamonds,
+            Clubs
+        }
+
+        enum Rank
+        {
+            Ace = 11,
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            Jack = 2,
+            Queen = 3,
+            King = 4
+        }
+
+        public static void Start()
+        {
+            int playerScore = 0;
+            int computerScore = 0;
+            int gamesPlayed = 0;
+            int playerWins = 0;
+            int computerWins = 0;
+            bool continuePlaying = true;
+            Random random = new Random();
+
+            Console.WriteLine("Welcome to the game of 21!");
+
+            while (continuePlaying)
+            {
+                gamesPlayed++;
+
+                Console.WriteLine("\nNew Game:");
+
+                List<string> deck = CreateDeck();
+
+                ShuffleDeck(deck, random);
+
+                List<string> playerHand = new List<string>();
+                List<string> computerHand = new List<string>();
+
+                DealInitialCards(deck, playerHand, computerHand);
+
+                bool playerTurn = DetermineFirstPlayer();
+
+                Console.WriteLine("\nYour turn!");
+
+                playerScore = CalculateScore(playerHand);
+                Console.WriteLine($"Your score: {playerScore}");
+
+                while (playerTurn)
+                {
+                    Console.WriteLine("Do you want another card? (Y/N)");
+                    string response = Console.ReadLine().ToUpper();
+
+                    if (response == "Y")
+                    {
+                        DealCard(deck, playerHand);
+                        playerScore = CalculateScore(playerHand);
+                        Console.WriteLine($"Your score: {playerScore}");
+
+                        if (playerScore > 21)
+                        {
+                            Console.WriteLine("You have exceeded 21. You lose!");
+                            computerWins++;
+                            playerTurn = false;
+                        }
+                        else if (playerScore == 21)
+                        {
+                            Console.WriteLine("Congratulations! You scored 21. You win!");
+                            playerWins++;
+                            playerTurn = false;
+                        }
+                    }
+                    else if (response == "N")
+                    {
+                        Console.WriteLine("You chose to stop. It's computer's turn now.");
+                        playerTurn = false;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter Y or N.");
+                    }
+                }
+
+                if (playerScore <= 21)
+                {
+                    Console.WriteLine("\nComputer's turn!");
+
+                    computerScore = CalculateScore(computerHand);
+
+                    while (computerScore < playerScore && computerScore < 21)
+                    {
+                        DealCard(deck, computerHand);
+                        computerScore = CalculateScore(computerHand);
+                    }
+
+                    Console.WriteLine($"Computer score: {computerScore}");
+
+                    if (computerScore > 21)
+                    {
+                        Console.WriteLine("Computer has exceeded 21. You win!");
+                        playerWins++;
+                    }
+                    else if (computerScore == 21)
+                    {
+                        Console.WriteLine("Computer scored 21. Computer wins!");
+                        computerWins++;
+                    }
+                    else
+                    {
+                        if (computerScore > playerScore)
+                        {
+                            Console.WriteLine("Computer has a higher score. Computer wins!");
+                            computerWins++;
+                        }
+                        else if (playerScore > computerScore)
+                        {
+                            Console.WriteLine("You have a higher score. You win!");
+                            playerWins++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("It's a tie!");
+                        }
+                    }
+                }
+
+                Console.WriteLine("\nGame over!");
+                Console.WriteLine($"Player wins: {playerWins}");
+                Console.WriteLine($"Computer wins: {computerWins}");
+
+                Console.WriteLine("\nDo you want to play again? (Y/N)");
+                string playAgainResponse = Console.ReadLine().ToUpper();
+                continuePlaying = (playAgainResponse == "Y");
+            }
+
+            Console.WriteLine("\nThank you for playing!");
+
+            Console.WriteLine("\nOverall game statistics:");
+            Console.WriteLine($"Games played: {gamesPlayed}");
+            Console.WriteLine($"Player wins: {playerWins}");
+            Console.WriteLine($"Computer wins: {computerWins}");
+        }
+
+        static List<string> CreateDeck()
+        {
+            List<string> deck = new List<string>();
+
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (Rank rank in Enum.GetValues(typeof(Rank)))
+                {
+                    deck.Add($"{rank} of {suit}");
+                }
+            }
+
+            return deck;
+        }
+
+        static void ShuffleDeck(List<string> deck, Random random)
+        {
+            int n = deck.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = random.Next(n + 1);
+                string temp = deck[k];
+                deck[k] = deck[n];
+                deck[n] = temp;
+            }
+        }
+
+        static void DealInitialCards(List<string> deck, List<string> playerHand, List<string> computerHand)
+        {
+            DealCard(deck, playerHand);
+            DealCard(deck, computerHand);
+            DealCard(deck, playerHand);
+            DealCard(deck, computerHand);
+
+            Console.WriteLine("\nPlayer's hand:");
+            foreach (string card in playerHand)
+            {
+                Console.WriteLine(card);
+            }
+
+            Console.WriteLine("\nComputer's hand:");
+            Console.WriteLine(computerHand[0]);
+            Console.WriteLine("Face down card");
+        }
+
+        static void DealCard(List<string> deck, List<string> hand)
+        {
+            string card = deck[0];
+            deck.RemoveAt(0);
+            hand.Add(card);
+        }
+
+        static bool DetermineFirstPlayer()
+        {
+            Console.WriteLine("Who receives the first cards? (P/C)");
+            string response = Console.ReadLine().ToUpper();
+            return (response == "P");
+        }
+
+        static int CalculateScore(List<string> hand)
+        {
+            int score = 0;
+
+            foreach (string card in hand)
+            {
+                Rank rank = (Rank)Enum.Parse(typeof(Rank), card.Split()[0]);
+                score += (int)rank;
+            }
+
+            return score;
+        }
+    }
+}
