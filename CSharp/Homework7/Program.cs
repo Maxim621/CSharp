8c52c53cdce39da00bcdb215e3b5cb1069868fbc
ADM <parfemax@gmail.com>
2023-08-01 22:02:09 +0300

    Redone Homework7

--- CSharp/Homework7/Program.cs
+++ CSharp/Homework7/Program.cs
@@ -26,23 +26,23 @@ public class Program
     };
 
         // Завдання 1
-        Console.WriteLine(string.Join(", ", data.Where(item => !(item is ArtObject))));
+        Console.WriteLine(string.Join(", ", data.Where(item => item is not ArtObject)));
 
         // Завдання 2
-        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(film => film.Actors).Select(actor => actor.Name)));
+        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(film => film.Actors).Select(actor => actor.Name).Distinct()));
 
         // Завдання 3
-        Console.WriteLine(data.OfType<Film>().SelectMany(film => film.Actors).Count(actor => actor.Birthdate.Month == 8));
+        Console.WriteLine(data.OfType<Film>().SelectMany(film => film.Actors).Where(actor => actor.Birthdate.Month == 8).Select(actor => actor.Name).Distinct().Count());
 
         // Завдання 4
-        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(film => film.Actors).OrderByDescending(actor => actor.Birthdate).Take(2).Select(actor => actor.Name)));
+        Console.WriteLine(string.Join(", ", data.OfType<Film>().SelectMany(film => film.Actors).OrderBy(actor => actor.Birthdate).Take(2).Select(actor => actor.Name)));
 
         // Завдання 5
         Console.WriteLine(string.Join(", ", data.OfType<Book>().GroupBy(book => book.Author).Select(group => $"{group.Key}: {group.Count()}")));
 
         // Завдання 6
-        Console.WriteLine($"Books per author: {data.OfType<Book>().GroupBy(book => book.Author).Select(group => $"{group.Key}: {group.Count()}").FirstOrDefault()}");
-        Console.WriteLine($"Films per director: {data.OfType<Film>().GroupBy(film => film.Author).Select(group => $"{group.Key}: {group.Count()}").FirstOrDefault()}");
+        Console.WriteLine($"Books per author: {data.OfType<Book>().GroupBy(book => book.Author).Select(group => $"{group.Key}: {group.Count()}").FirstOrDefault()} \n" +
+                  $"Films per director: {data.OfType<Film>().GroupBy(film => film.Author).Select(group => $"{group.Key}: {group.Count()}").FirstOrDefault()}");
 
         // Завдання 7
         Console.WriteLine(data.OfType<Film>().SelectMany(film => film.Actors).SelectMany(actor => actor.Name.ToLower()).Distinct().Count());
@@ -51,31 +51,17 @@ public class Program
         Console.WriteLine(string.Join(", ", data.OfType<Book>().OrderBy(book => book.Author).ThenBy(book => book.Pages).Select(book => $"{book.Name} ({book.Author}): {book.Pages} pages")));
 
         // Завдання 9
-        var actorName = "Matt Damon";
-        Console.WriteLine($"{actorName}'s films:");
-        Console.WriteLine(string.Join(", ", data.OfType<Film>().Where(film => film.Actors.Any(actor => actor.Name == actorName)).Select(film => film.Name)));
+        Console.WriteLine(string.Join("\n", data.OfType<Film>().SelectMany(film => film.Actors, (film, actor) => $"{actor.Name}: {film.Name}")));
 
         // Завдання 10
-        var totalPageCount = data.OfType<Book>().Sum(book => book.Pages);
-        var intValues = data.OfType<IEnumerable<int>>().SelectMany(seq => seq).Sum();
-        Console.WriteLine($"Total page count: {totalPageCount}");
-        Console.WriteLine($"Sum of all int values: {intValues}");
+        Console.WriteLine($"Total page count: {data.OfType<Book>().Sum(book => book.Pages)} \n" +
+                  $"Sum of all int values: {data.OfType<IEnumerable<int>>().SelectMany(seq => seq).Sum()}");
 
         // Завдання 11
-        var authorBooks = data.OfType<Book>().GroupBy(book => book.Author).ToDictionary(group => group.Key, group => group.ToList());
-        foreach (var author in authorBooks)
-        {
-            Console.WriteLine($"Author: {author.Key}");
-            Console.WriteLine(string.Join(", ", author.Value.Select(book => book.Name)));
-        }
+        Console.WriteLine(string.Join("\n", data.OfType<Book>().GroupBy(book => book.Author).ToDictionary(group => group.Key, group => string.Join(", ", group.Select(book => book.Name)))));
 
         // Завдання 12
-        var excludedActorNames = data.OfType<string>().ToList();
-        var mattDamonFilms = data.OfType<Film>().Where(film => film.Actors.All(actor => !excludedActorNames.Contains(actor.Name)) && film.Actors.Any(actor => actor.Name == "Matt Damon"));
-        foreach (var film in mattDamonFilms)
-        {
-            Console.WriteLine(film.Name);
-        }
+        Console.WriteLine(string.Join("\n", data.OfType<Film>().Where(film => film.Actors.Any(actor => actor.Name == "Matt Damon") && !data.OfType<string>().Any(name => film.Actors.Any(actor => actor.Name == name)))));
 
     }
 }
