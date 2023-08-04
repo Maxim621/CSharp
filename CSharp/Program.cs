b3afca83bfd0c69d89552d0f53cc0771d7ff9947
ADM <parfemax@gmail.com>
2023-08-04 22:54:10 +0300

    Redone2 Homeowrk6

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -24,18 +24,19 @@ class Program
         Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Any(s => s == "abb"));
 
         // Отримати найдовше слово в рядку "aaa;xabbx;abb;ccc;dap"
-        Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Aggregate("", (max, next) => next.Length > max.Length ? next : max));
+        Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').MaxBy(s => s.Length));
 
         // Обчислити середню довжину слова в рядку "aaa;xabbx;abb;ccc;dap"
         Console.WriteLine("aaa;xabbx;abb;ccc;dap".Split(';').Average(s => s.Length));
 
         // Вивести найкоротше слово в рядку "aaa;xabbx;abb;ccc;dap;zh" у зворотному порядку
-        Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').FirstOrDefault(s => s.StartsWith("aa") && !s.Skip(2).Any(c => c != 'b')) != null);
+        Console.WriteLine(new string("aaa;xabbx;abb;ccc;dap;zh".Split(';').MinBy(s => s.Length).Reverse().ToArray()));
 
         // Вивести true, якщо в першому слові, яке починається з "aa", усі літери "b" (За винятком "аа"), інакше false
-        Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').First(s => s.StartsWith("aa") && !s.Substring(2).Contains("b")) != null);
+        Console.WriteLine("baaa;aabb;aaa;xabbx;abb;ccc;dap;zh".Split(';').FirstOrDefault(s => s.StartsWith("aa") && s.Skip(2).All(c => c == 'b')) != null);
 
         // Вивести останнє слово в послідовності, за винятком перших двох елементів, які закінчуються на "bb" (використовуйте послідовність із 10 завдання)
-        Console.WriteLine(string.Join(";", "aaa;xabbx;abb;ccc;dap".Split(';').Skip(2).Where(s => s.EndsWith("bb")).TakeLast(1)));
+        Console.WriteLine(string.Join(";", "aaa;xabbx;abb;ccc;dap".Split(';').Where(s => s.EndsWith("bb")).Skip(2).TakeLast(1)));
+
     }
 }
