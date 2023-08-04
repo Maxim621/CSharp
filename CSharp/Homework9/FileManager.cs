77d0145a261f13628e9cf549b0f251fb2112f5dc
ADM <parfemax@gmail.com>
2023-08-04 23:07:26 +0300

    Redone Homework9

--- CSharp/Homework9/FileManager.cs
+++ CSharp/Homework9/FileManager.cs
@@ -3,95 +3,44 @@ using System.IO;
 
 namespace CSharp.Homework9
 {
-    public class FileManager
+    class FileManager
     {
         private string currentDirectory;
+        private UserInterface ui;
+
+        public FileManager()
+        {
+            currentDirectory = Directory.GetCurrentDirectory();
+            ui = new UserInterface();
+        }
 
         public void Run()
         {
-            currentDirectory = Directory.GetCurrentDirectory();
-            DisplayDirectoryContents();
-
             while (true)
             {
+                ui.DisplayDirectoryContents(currentDirectory);
+
                 ConsoleKeyInfo keyInfo = Console.ReadKey(true);
                 if (keyInfo.Key == ConsoleKey.Escape)
                     break;
 
                 if (keyInfo.Key == ConsoleKey.Enter)
                 {
-                    if (IsDirectorySelected())
+                    if (ui.IsDirectorySelected())
                     {
-                        string selectedDirectory = GetSelectedDirectory();
+                        string selectedDirectory = ui.GetSelectedDirectory();
                         currentDirectory = selectedDirectory;
-                        DisplayDirectoryContents();
                     }
                     else
                     {
-                        string selectedFile = GetSelectedFile();
+                        string selectedFile = ui.GetSelectedFile();
                         if (IsTextFile(selectedFile))
                             ReadTextFile(selectedFile);
                         else
-                            Console.WriteLine("Cannot open the selected file. Only text files are supported.");
+                            ui.DisplayMessage("Cannot open the selected file. Only text files are supported.");
                     }
                 }
             }
-        }
-
-        private void DisplayDirectoryContents()
-        {
-            Console.Clear();
-            Console.WriteLine("Current Directory: " + currentDirectory);
-            Console.WriteLine();
-
-            string[] directories = Directory.GetDirectories(currentDirectory);
-            string[] files = Directory.GetFiles(currentDirectory);
-
-            Console.WriteLine("Directories:");
-            foreach (string directory in directories)
-                Console.WriteLine("[DIR] " + Path.GetFileName(directory));
-
-            Console.WriteLine();
-
-            Console.WriteLine("Files:");
-            foreach (string file in files)
-                Console.WriteLine("[FILE] " + Path.GetFileName(file));
-
-            Console.WriteLine();
-            Console.WriteLine("Press Enter to open a directory or file. Press Esc to exit.");
-        }
-
-        private bool IsDirectorySelected()
-        {
-            int cursorTop = Console.CursorTop;
-            Console.SetCursorPosition(0, cursorTop - 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, cursorTop - 1);
-
-            string selectedItem = Console.ReadLine();
-            return selectedItem.StartsWith("[DIR]");
-        }
-
-        private string GetSelectedDirectory()
-        {
-            int cursorTop = Console.CursorTop;
-            Console.SetCursorPosition(0, cursorTop - 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, cursorTop - 1);
-
-            string selectedDirectory = Console.ReadLine();
-            return Path.Combine(currentDirectory, selectedDirectory.Trim().Remove(0, 6));
-        }
-
-        private string GetSelectedFile()
-        {
-            int cursorTop = Console.CursorTop;
-            Console.SetCursorPosition(0, cursorTop - 1);
-            Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, cursorTop - 1);
-
-            string selectedFile = Console.ReadLine();
-            return Path.Combine(currentDirectory, selectedFile.Trim().Remove(0, 7));
         }
 
         private bool IsTextFile(string filePath)
@@ -105,19 +54,17 @@ namespace CSharp.Homework9
             try
             {
                 string fileContents = File.ReadAllText(filePath);
-                Console.Clear();
-                Console.WriteLine("File Contents:");
-                Console.WriteLine();
-                Console.WriteLine(fileContents);
-                Console.WriteLine();
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey(true);
+                ui.DisplayFileContents(fileContents);
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                while (keyInfo.Key != ConsoleKey.Escape)
+                {
+                    keyInfo = Console.ReadKey(true);
+                }
             }
             catch (Exception ex)
             {
-                Console.WriteLine("An error occurred while reading the file: " + ex.Message);
-                Console.WriteLine("Press any key to continue.");
-                Console.ReadKey(true);
+                ui.DisplayMessage("An error occurred while reading the file: " + ex.Message);
             }
         }
     }
---
+++ CSharp/Homework9/UserInterface.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace CSharp.Homework9
+{
+    class UserInterface
+    {
+        public void DisplayDirectoryContents(string currentDirectory)
+        {
+            Console.Clear();
+            Console.WriteLine("Current Directory: " + currentDirectory);
+            Console.WriteLine();
+
+            string[] directories = Directory.GetDirectories(currentDirectory);
+            string[] files = Directory.GetFiles(currentDirectory);
+
+            Console.WriteLine("Directories:");
+            foreach (string directory in directories)
+                Console.WriteLine("[DIR] " + Path.GetFileName(directory));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Files:");
+            foreach (string file in files)
+                Console.WriteLine("[FILE] " + Path.GetFileName(file));
+
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to open a directory or file. Press Esc to exit.");
+        }
+
+        public bool IsDirectorySelected()
+        {
+            int cursorTop = Console.CursorTop;
+            Console.SetCursorPosition(0, cursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, cursorTop - 1);
+
+            string selectedItem = Console.ReadLine();
+            return selectedItem.StartsWith("[DIR]");
+        }
+
+        public string GetSelectedDirectory()
+        {
+            int cursorTop = Console.CursorTop;
+            Console.SetCursorPosition(0, cursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, cursorTop - 1);
+
+            string selectedDirectory = Console.ReadLine();
+            return Path.Combine(Directory.GetCurrentDirectory(), selectedDirectory.Trim().Remove(0, 6));
+        }
+
+        public string GetSelectedFile()
+        {
+            int cursorTop = Console.CursorTop;
+            Console.SetCursorPosition(0, cursorTop - 1);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, cursorTop - 1);
+
+            string selectedFile = Console.ReadLine();
+            return Path.Combine(Directory.GetCurrentDirectory(), selectedFile.Trim().Remove(0, 7));
+        }
+
+        public void DisplayFileContents(string fileContents)
+        {
+            Console.Clear();
+            Console.WriteLine("File Contents:");
+            Console.WriteLine();
+            Console.WriteLine(fileContents);
+            Console.WriteLine();
+            Console.WriteLine("Press Esc to return.");
+        }
+
+        public void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
