0fdd055dc29fa9c0d0f724deccfe1662060830cf
ADM <parfemax@gmail.com>
2023-08-06 23:09:17 +0300

    Redone Homework11

--- CSharp/Homework11/CityGenerator/CityGenerator.cs
+++ CSharp/Homework11/CityGenerator/CityGenerator.cs
@@ -1,12 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace CSharp.Homework11
+namespace CHarp.Homework11.CityGenerator
 {
-    internal class CityGenerator
+    public class Program
     {
+        static readonly string CitiesDataFile = "cities.txt";
+
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: CityGenerator <outputFilePath> <numberOfRows>");
+                return;
+            }
+
+            string outputFilePath = args[0];
+            int numberOfRows = int.Parse(args[1]);
+
+            List<string> citiesData = File.ReadAllLines(CitiesDataFile).ToList();
+
+            GenerateCitiesAndSaveToFile(citiesData, outputFilePath, numberOfRows);
+
+            Console.WriteLine($"Generated {numberOfRows} rows and saved to {outputFilePath}.");
+        }
+
+        static void GenerateCitiesAndSaveToFile(List<string> citiesData, string outputFilePath, int numberOfRows)
+        {
+            Random random = new Random();
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                for (int i = 0; i < numberOfRows; i++)
+                {
+                    string city = citiesData[random.Next(citiesData.Count)];
+                    string country = citiesData[random.Next(citiesData.Count)];
+                    string region = citiesData[random.Next(citiesData.Count)];
+                    double area = random.Next(100, 1000) + random.NextDouble();
+                    int population = random.Next(1000, 1000000);
+
+                    string line = $"{city}:{area:F2};{population};{country}({region})";
+                    writer.WriteLine(line);
+                }
+            }
+        }
     }
 }
--- CSharp/Homework11/CityParser/CityParser.cs
+++ CSharp/Homework11/CityParser/CityParser.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
-namespace CSharp.Homework11
+namespace CSharp.Homework11.CityParser
 {
-    internal class CityParser
+    public class Program
     {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: CityParser <inputFilePath> <outputFilePath>");
+                return;
+            }
+
+            string inputFilePath = args[0];
+            string outputFilePath = args[1];
+
+            List<string> citiesData = ParseCitiesFromFile(inputFilePath);
+            WriteCitiesToFile(citiesData, outputFilePath);
+
+            Console.WriteLine($"Parsed {citiesData.Count} cities and saved to {outputFilePath}.");
+        }
+
+        static List<string> ParseCitiesFromFile(string inputFilePath)
+        {
+            List<string> citiesData = new List<string>();
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    citiesData.Add(line);
+                }
+            }
+
+            return citiesData;
+        }
+
+        static void WriteCitiesToFile(List<string> citiesData, string outputFilePath)
+        {
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                foreach (var cityData in citiesData)
+                {
+                    writer.WriteLine(cityData);
+                }
+            }
+        }
     }
 }
