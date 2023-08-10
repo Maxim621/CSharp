423526bbdd7e89fe266d3b39856024f5ec209cf2
ADM <parfemax@gmail.com>
2023-08-10 22:27:29 +0300

    Homework16

---
+++ CSharp/Homework16/ClientChat/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp.Homework16.ClientChat
+{
+    internal class Client
+    {
+    }
+}
