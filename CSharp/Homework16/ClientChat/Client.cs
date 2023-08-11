19b22288eef608cee7ae718dcc4bae4d20653f82
ADM <parfemax@gmail.com>
2023-08-11 20:04:21 +0300

    Homework16

--- CSharp/Homework16/ClientChat/Client.cs
+++ CSharp/Homework16/ClientChat/Client.cs
@@ -1,12 +1,42 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSharp.Homework16.ClientChat
 {
-    internal class Client
+    class Client
     {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient("127.0.0.1", 12345);
+            Console.WriteLine("Connected to server.");
+
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8) { AutoFlush = true };
+
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            writer.WriteLine(name);
+
+            Thread receiveThread = new Thread(() => ReceiveMessages(reader));
+            receiveThread.Start();
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                writer.WriteLine(message);
+            }
+        }
+
+        static void ReceiveMessages(StreamReader reader)
+        {
+            while (true)
+            {
+                string message = reader.ReadLine();
+                Console.WriteLine(message);
+            }
+        }
     }
 }
--- CSharp/Homework16/ServerChat/Server.cs
+++ CSharp/Homework16/ServerChat/Server.cs
@@ -1,12 +1,80 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace CSharp.Homework16.ServerChat
 {
-    internal class Server
+    class Server
     {
+        private static ConcurrentDictionary<string, StreamWriter> clients = new ConcurrentDictionary<string, StreamWriter>();
+
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 12345);
+            server.Start();
+            Console.WriteLine("Server started.");
+
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+
+                StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8) { AutoFlush = true };
+
+                Thread clientThread = new Thread(() => HandleClient(client, writer));
+                clientThread.Start();
+            }
+        }
+
+        static void HandleClient(TcpClient client, StreamWriter writer)
+        {
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            string name = reader.ReadLine();
+            clients.TryAdd(name, writer);
+
+            while (true)
+            {
+                try
+                {
+                    string message = reader.ReadLine();
+                    Console.WriteLine("Received: " + message);
+
+                    if (message.StartsWith("@"))
+                    {
+                        string[] parts = message.Split(' ', 2);
+                        if (parts.Length >= 2 && clients.TryGetValue(parts[0].Substring(1), out var privateWriter))
+                        {
+                            privateWriter.WriteLine($"[Private from {name}]: {parts[1]}");
+                        }
+                        else
+                        {
+                            writer.WriteLine("User not found or invalid format.");
+                        }
+                    }
+                    else
+                    {
+                        foreach (var clientWriter in clients.Values)
+                        {
+                            if (clientWriter != writer)
+                            {
+                                clientWriter.WriteLine($"[{name}]: {message}");
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Client disconnected.");
+                    clients.TryRemove(name, out _);
+                    break;
+                }
+            }
+
+            client.Close();
+        }
     }
 }
