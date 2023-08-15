1bc95ea623f99402b0c8145305f340eebcfecd47
ADM <parfemax@gmail.com>
2023-08-15 22:55:17 +0300

    Redone Homework16

--- CSharp/Homework16/ServerChat/Server.cs
+++ CSharp/Homework16/ServerChat/Server.cs
@@ -6,75 +6,95 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-namespace CSharp.Homework16.ServerChat
+class ClientHandler
 {
-    class Server
+    private TcpClient client;
+    private string name;
+    private StreamWriter writer;
+    private ConcurrentDictionary<string, ClientHandler> clients;
+
+    public ClientHandler(TcpClient client, string name, ConcurrentDictionary<string, ClientHandler> clients)
     {
-        private static ConcurrentDictionary<string, StreamWriter> clients = new ConcurrentDictionary<string, StreamWriter>();
+        this.client = client;
+        this.name = name;
+        this.clients = clients;
+        writer = new StreamWriter(client.GetStream(), Encoding.UTF8) { AutoFlush = true };
+    }
 
-        static void Main(string[] args)
-        {
-            TcpListener server = new TcpListener(IPAddress.Any, 12345);
-            server.Start();
-            Console.WriteLine("Server started.");
+    public void Start()
+    {
+        Thread clientThread = new Thread(HandleClient);
+        clientThread.Start();
+    }
 
-            while (true)
-            {
-                TcpClient client = server.AcceptTcpClient();
-                Console.WriteLine("Client connected.");
-
-                StreamWriter writer = new StreamWriter(client.GetStream(), Encoding.UTF8) { AutoFlush = true };
-
-                Thread clientThread = new Thread(() => HandleClient(client, writer));
-                clientThread.Start();
-            }
-        }
-
-        static void HandleClient(TcpClient client, StreamWriter writer)
+    private void HandleClient()
+    {
+        try
         {
             StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
-            string name = reader.ReadLine();
-            clients.TryAdd(name, writer);
 
             while (true)
             {
-                try
-                {
-                    string message = reader.ReadLine();
-                    Console.WriteLine("Received: " + message);
+                string message = reader.ReadLine();
+                Console.WriteLine("Received: " + message);
 
-                    if (message.StartsWith("@"))
+                if (message.StartsWith("@"))
+                {
+                    string[] parts = message.Split(' ', 2);
+                    if (parts.Length >= 2 && clients.TryGetValue(parts[0].Substring(1), out var privateClient))
                     {
-                        string[] parts = message.Split(' ', 2);
-                        if (parts.Length >= 2 && clients.TryGetValue(parts[0].Substring(1), out var privateWriter))
-                        {
-                            privateWriter.WriteLine($"[Private from {name}]: {parts[1]}");
-                        }
-                        else
-                        {
-                            writer.WriteLine("User not found or invalid format.");
-                        }
+                        privateClient.writer.WriteLine($"[Private from {name}]: {parts[1]}");
                     }
                     else
                     {
-                        foreach (var clientWriter in clients.Values)
+                        writer.WriteLine("User not found or invalid format.");
+                    }
+                }
+                else
+                {
+                    foreach (var client in clients.Values)
+                    {
+                        if (client != this)
                         {
-                            if (clientWriter != writer)
-                            {
-                                clientWriter.WriteLine($"[{name}]: {message}");
-                            }
+                            client.writer.WriteLine($"[{name}]: {message}");
                         }
                     }
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("Client disconnected.");
-                    clients.TryRemove(name, out _);
-                    break;
-                }
             }
-
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Client disconnected.");
+            clients.TryRemove(name, out _);
+        }
+        finally
+        {
             client.Close();
+        }
+    }
+}
+
+class Server
+{
+    private static ConcurrentDictionary<string, ClientHandler> clients = new ConcurrentDictionary<string, ClientHandler>();
+
+    static void Main(string[] args)
+    {
+        TcpListener server = new TcpListener(IPAddress.Any, 12345);
+        server.Start();
+        Console.WriteLine("Server started.");
+
+        while (true)
+        {
+            TcpClient client = server.AcceptTcpClient();
+            Console.WriteLine("Client connected.");
+
+            StreamReader reader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            string name = reader.ReadLine();
+
+            ClientHandler clientHandler = new ClientHandler(client, name, clients);
+            clients.TryAdd(name, clientHandler);
+            clientHandler.Start();
         }
     }
 }
